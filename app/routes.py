48893974253f65f2b1240284e2dbf790b2c06448
Meirlan <aitmuhamet.meirlan28@mail.ru>
2025-06-10 09:34:14 +0500

    logging2

--- app/routes.py
+++ app/routes.py
@@ -3,20 +3,35 @@ from flask import Blueprint, request, jsonify, render_template, redirect, url_fo
 from app import db
 from app.models import Animal
 import time
+from pythonjsonlogger import jsonlogger
+from datetime import datetime
 
 bp = Blueprint('main', __name__)
 
-# Настройка логгера
-logger = logging.getLogger(__name__)
+# Настройка логгера с JSON форматером
+logger = logging.getLogger('myapp')
 logger.setLevel(logging.INFO)
 
-# Можно настроить обработчик, чтобы выводить в консоль
 console_handler = logging.StreamHandler()
-console_handler.setLevel(logging.INFO)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = jsonlogger.JsonFormatter(
+    '%(host)s %(user_identifier)s %(datetime)s %(method)s %(request)s %(protocol)s %(status)s %(bytes)s %(referer)s %(message)s'
+)
 console_handler.setFormatter(formatter)
 logger.addHandler(console_handler)
 
+def get_log_data(status=200, bytes_=0, message="Request processed"):
+    return {
+        "host": request.remote_addr or "",
+        "user_identifier": "anonymous",  # если есть авторизация — подставь сюда
+        "datetime": datetime.utcnow().strftime("%d/%b/%Y:%H:%M:%S +0000"),
+        "method": request.method,
+        "request": request.path,
+        "protocol": request.environ.get('SERVER_PROTOCOL'),
+        "status": status,
+        "bytes": bytes_,
+        "referer": request.referrer or "",
+        "log_message": message
+    }
 
 @bp.route('/')
 def index():
@@ -31,6 +46,7 @@ def add_animal():
         new_animal = Animal(name=name, species=species)
         db.session.add(new_animal)
         db.session.commit()
+        logger.info("", extra=get_log_data(message=f"Added animal: Name={name}, Species={species}"))
         return redirect(url_for('main.index'))
     return render_template('add_animal.html')
 
@@ -39,7 +55,7 @@ def delete_animal(id):
     animal = Animal.query.get_or_404(id)
     
     # Логируем информацию об удалении
-    logger.info(f"User IP {request.remote_addr} deleted animal: ID={animal.id}, Name={animal.name}, Species={animal.species}")
+    logger.info("", extra=get_log_data(message=f"Deleted animal: ID={animal.id}, Name={animal.name}, Species={animal.species}"))
     
     db.session.delete(animal)
     db.session.commit()
@@ -52,5 +68,6 @@ def error_simulator(code):
         time.sleep(seconds_sleep)  # Задержка
     
     if code in [400, 403, 404, 409, 500]:
+        logger.info("", extra=get_log_data(status=code, message=f"Simulated error {code}"))
         abort(code)
     return f"Unsupported code: {code}", 400
