c239656decc65183969938df18b4e8c69f543294
Meirlan <aitmuhamet.meirlan28@mail.ru>
2025-06-10 08:46:51 +0500

    logging

--- app/routes.py
+++ app/routes.py
@@ -1,10 +1,22 @@
-#routes.py
+import logging
 from flask import Blueprint, request, jsonify, render_template, redirect, url_for, abort
 from app import db
 from app.models import Animal
 import time
 
 bp = Blueprint('main', __name__)
+
+# Настройка логгера
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+# Можно настроить обработчик, чтобы выводить в консоль
+console_handler = logging.StreamHandler()
+console_handler.setLevel(logging.INFO)
+formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+console_handler.setFormatter(formatter)
+logger.addHandler(console_handler)
+
 
 @bp.route('/')
 def index():
@@ -19,16 +31,19 @@ def add_animal():
         new_animal = Animal(name=name, species=species)
         db.session.add(new_animal)
         db.session.commit()
-        return redirect(url_for('main.index'))  # Перенаправляем на главную страницу
+        return redirect(url_for('main.index'))
     return render_template('add_animal.html')
 
 @bp.route('/delete/<int:id>', methods=['POST'])
 def delete_animal(id):
     animal = Animal.query.get_or_404(id)
+    
+    # Логируем информацию об удалении
+    logger.info(f"User IP {request.remote_addr} deleted animal: ID={animal.id}, Name={animal.name}, Species={animal.species}")
+    
     db.session.delete(animal)
     db.session.commit()
     return redirect(url_for('main.index'))
-
 
 @bp.route('/status/<int:code>')
 def error_simulator(code):
